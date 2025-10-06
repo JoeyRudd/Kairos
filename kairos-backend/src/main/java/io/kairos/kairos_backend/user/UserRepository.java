5c7e5447b44017c08165dfbab106a74c42a5e94a
Joey Rudd <emailjoeyrudd@gmail.com>
2025-10-06 14:13:05 -0400

    Change user repository to extend jpa instead of crud so findall returns list instead of iterable

--- kairos-backend/src/main/java/io/kairos/kairos_backend/user/UserRepository.java
+++ kairos-backend/src/main/java/io/kairos/kairos_backend/user/UserRepository.java
@@ -1,10 +1,11 @@
 package io.kairos.kairos_backend.user;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findUserByEmail(String email);
     Boolean existsBy(String email);
--- kairos-backend/src/main/java/io/kairos/kairos_backend/user/UserService.java
+++ kairos-backend/src/main/java/io/kairos/kairos_backend/user/UserService.java
@@ -1,10 +1,30 @@
 package io.kairos.kairos_backend.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
 
+    @Autowired
     private UserRepository userRepository;
 
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    Optional<User> findUserByEmail(String email) {
+        return userRepository.findUserByEmail(email);
+    }
+
+    Optional<User> findUserById(long id) {
+        return userRepository.findById(id);
+    }
+
+    public List<User> findAllUsers() {
+        return userRepository.findAll();
+    }
 }
